0ffd1fd91156f15613935e586ae766ff45313635
shbunch <shawnbunch@gmail.com>
2023-08-21 03:06:11 -0500

    Added some logic to the database methods

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    // Create a new row in the 'content' table with the provided content
+    await content.create({ text: content });
+    console.log('Content added to the database');
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  }
+  console.error('putDb not implemented');
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    // Retrieve all rows from the 'content' table
+    const allContent = await Content.findAll();
+    return allContent.map(item => item.text); // Extracting the 'text' field from each item
+  } catch (error) {
+    console.error('Error retrieving content from the database:', error);
+    return []; // Return an empty array or handle the error accordingly
+  }
+  console.error('getDb not implemented');
+}
 
 initdb();
+
